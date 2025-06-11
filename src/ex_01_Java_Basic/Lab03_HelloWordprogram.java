9358297484c68918648b529a7cdb012c7064ed35
vijayakadgaraju0616 <vijaya.kadgaraju@gmail.com>
2025-06-11 23:32:47 +0530

    helloworld

---
+++ src/ex_01_Java_Basic/Lab03_HelloWordprogram.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab03_HelloWordprogram {
+
+    public static void main(String[] args){
+        System.out.println("Hello World,this is java ATB X13");
+    }
+}
