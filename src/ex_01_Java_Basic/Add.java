13feedeb4600b1dc0d4ccb041400cbb967f7bb4b
vijayakadgaraju0616 <vijaya.kadgaraju@gmail.com>
2025-06-12 00:24:42 +0530

    Add

---
+++ src/ex_01_Java_Basic/Add.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Add{
+        public static void main(String[] args) {
+            int i=10;
+            int j=30;
+            System.out.println(i+j);
+
+        }}
+
